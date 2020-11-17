d9e20a7c9576c00c986186545070d0a7875e5ab8
Daniel Demidko <demidko@farpost.com>
2020-11-17 20:26:25 +1000

    Вкидываем stl

--- Main.cpp
+++ Main.cpp
@@ -4,25 +4,42 @@
 #include <algorithm>
 #include <sstream>
 #include <iterator>
-#include <regex>
 #include <unordered_map>
-
-using namespace std::string_view_literals;
+#include <map>
 
 
 int compress(std::string_view filename) {
+
+  std::unordered_map<std::string, int> unsortedFrequency;
   std::ifstream file(filename);
-  std::unordered_map<std::string, int> frequency;
   for (std::string buf; std::getline(file, buf);) {
     std::istringstream line(buf);
     auto fullUrl = std::next(std::istream_iterator<std::string>(line), 9);
     auto shortUrl = std::istringstream(fullUrl->substr(0, fullUrl->find('?')));
     for (std::string term; std::getline(shortUrl, term, '/');) {
-      ++frequency[term];
+      ++unsortedFrequency[term];
     }
   }
-  for (auto[term, frequency]: frequency) {
-    std::cout << term << " -> " << frequency << '\n';
+
+
+  std::multimap<int, std::string> sortedFrequency;
+  std::transform(
+    unsortedFrequency.cbegin(),
+    unsortedFrequency.cend(),
+    std::inserter(sortedFrequency, sortedFrequency.begin()),
+    [](auto &&p) { return std::pair{std::move(p.second), p.first}; }
+  );
+
+  std::unordered_map<std::string, int> terms;
+  std::transform(
+    sortedFrequency.cbegin(),
+    sortedFrequency.cend(),
+    std::inserter(terms, terms.begin()),
+    [id = 0](auto &&p) mutable { return std::pair{std::move(p.second), ++id}; }
+  );
+
+  for (auto[term, id]: terms) {
+    std::cout << term << " -> " << id << '\n';
   }
   std::cout << std::flush;
   return 0;
@@ -33,14 +50,18 @@ int decompress(std::string_view filename) {
 }
 
 int main(int argc, char **argv) {
+
   if (argc == 2) {
     return compress(argv[1]);
   }
-  if (argc == 3 && "-d"sv == argv[1]) {
+
+  if (argc == 3 && std::string_view("-d") == argv[1]) {
     return decompress(argv[2]);
   }
+
   std::cerr << "Invalid options. Usage:\n"
                "  v2    [filename]      for compression\n"
                "  v2 -d [filename]      for decompression" << std::endl;
+
   return 1;
 }
