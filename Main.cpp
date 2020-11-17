8848da84fe0917a7062ff1af28e11876b929956c
Daniel Demidko <demidko@farpost.com>
2020-11-18 01:19:22 +1000

    Вкидываемся шаблонами

--- Main.cpp
+++ Main.cpp
@@ -13,7 +13,7 @@
 
 
 template<typename Input>
-bool compress(Input &&in) { // r&l-value universal ref
+void compress(Input &&in) { // r&l-value universal ref
 
   std::unordered_map<std::string, int> unsortedFrequency;
   for (std::string buf; std::getline(in, buf);) {
@@ -45,30 +45,29 @@ bool compress(Input &&in) { // r&l-value universal ref
     std::cout << term << " -> " << id << '\n';
   }
   std::cout << std::flush;
-  return true;
 }
 
 template<typename Input>
-bool decompress(Input &&in) { // r&l-value universal ref
+void decompress(Input &&in) { // r&l-value universal ref
   throw std::runtime_error("not implemented yet");
 }
 
 
-bool onCompressCalled(const std::vector<std::string> &logFiles) {
-
-}
-
-bool onDecompressCalled(const std::vector<std::string> &v2Files) {
-
+template<typename F>
+std::function<bool(const std::vector<std::string> &)> bind(F &&handler) {
+  return [&handler](auto &&files) {
+    if (files.empty()) return handler(std::cin);
+    for (auto &&file: files) handler(file);
+  };
 }
 
 int main(int argc, char **argv) {
   CLI::App v2{"Fast Farpost access logs compressor/decompressor", "v2"};
   v2
-    .add_option("-c,--compress", onCompressCalled, "Compress raw log [filename]")
+    .add_option("-c,--compress", bind(compress), "Compress raw log [filename]")
     ->expected(0, INT_MAX);
   v2
-    .add_option("-d,--decompress", onDecompressCalled, "Read compressed log [filename]")
+    .add_option("-d,--decompress", onDecompress, "Read compressed log [filename]")
     ->expected(0, INT_MAX);
   CLI11_PARSE(v2, argc, argv)
 }
