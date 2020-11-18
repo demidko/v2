6eedb46be7dc4c18f40a28169295df3f5a840453
Daniel Demidko <demidko@farpost.com>
2020-11-18 14:40:14 +1000

    Отвязываем синхронизацию от stdio

--- Main.cpp
+++ Main.cpp
@@ -88,6 +88,7 @@ std::function<bool(const std::vector<std::string> &)> by(F &&handler) {
 }
 
 int main(int argc, char **argv) {
+  std::ios::sync_with_stdio(false);
   CLI::App v2("Farpost access logs compressor/decompressor", "v2");
   v2.add_option("-c,--compress", by(compress), "Compress raw log files [filename, ...]")
     ->expected(0, INT_MAX);
