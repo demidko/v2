e532aaf10637fad32baf36528096b0df38b970b7
Daniel Demidko <demidko@farpost.com>
2020-11-20 10:51:44 +1000

    Выгружаем VLQ прямо в ostream

--- src/Vlq.h
+++ src/Vlq.h
@@ -17,26 +17,26 @@ namespace Vlq {
     explicit toVlqBits(T &&n) : value(n) {}
   };
 
-
   template<typename Number>
   std::ostream &operator<<(std::ostream &out, toVlqBits<Number> &&n) {
-    std::bitset<sizeof(Number) * 8> bits(n.value);
-    // пропускаем нулевые биты
-    auto bitIndex = bits.size();
-    for (; bitIndex < bits.size(); ++bitIndex) {
-      if (bits[bitIndex] == 1) { break; };
+    // переводим число в побитовое представление
+    std::bitset<sizeof(Number) * 8> number(n.value);
+    // пропускаем не заполненные биты
+    auto i = number.size();
+    for (; i < number.size(); ++i) {
+      if (number[i] == 1) { break; };
     }
     // двигаемся по значимым битам
-    for (; bitIndex < bits.size();) {
-      std::byte byte;
-      byte << (((bits.size() - bitIndex) > 7) ? 1 : 0);
-      auto limit = bitIndex + 7;
-      for (; bitIndex < limit && bitIndex < bits.size(); ++bitIndex) {
-        byte << bits[bitIndex];
+    for (; i < number.size();) {
+      // преобразуем
+      std::bitset<8> byte;
+      byte[0] = (((number.size() - i) > 7) ? 1 : 0);
+      for (auto j = 1; j < 8 && i < number.size(); ++j, ++i) {
+        byte[j] = number[i];
       }
-      // выгружаем по одному байту
-      out.write(reinterpret_cast<char *>(&byte), 1);
+      // и выгружаем по одному байту
+      auto symbol = (char) byte.to_ulong();
+      out.write(&symbol, 1);
     }
   }
-
 }
