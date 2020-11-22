d5be8233a5aa9ea8be676b0c24729f0af90e8b40
Daniel Demidko <demidko@farpost.com>
2020-11-22 20:52:11 +1000

    Запись по 7-битным октетам дополненным опциональнфыми нулями в конце

--- src/VlqOstream.h
+++ src/VlqOstream.h
@@ -14,11 +14,16 @@ struct VlqOstream {
 
   template<typename N>
   inline void encode(N number) {
-
+    // количество минимально необходимых для кодирования числа бит (брать по MSB-first)
     auto len = std::bit_width(number);
+    // количество полных 7 битных октетов в числе
     auto octets = std::ceil(len / 7.) - 1;
-    uint16_t i = 0;
+    // количество бит в последнем октете (<= 7)
+    auto rest = len - (octets * 7);
 
+    for ()
+
+      uint16_t i = 0;
     for (uint16_t j = 0; j < octets; ++j) {
       write(1);
       for (auto limit = i + 7; i < limit; ++i) { write(Bit::Get(number, i)); }
@@ -29,6 +34,21 @@ struct VlqOstream {
   }
 
 private:
+
+  template<typename N>
+  inline void encodeOctet(N number, uint16_t i, uint16_t bitWidth, bool isFull) {
+    // 1 маркирует полный 7 битный октет
+    // 0 маркирует последний, возможно менее чем 7 битный октет
+    write(isFull);
+    for (uint16_t limit = i + 7; i < limit; ++i) {
+      // явно заполняем нулями все что выходит за количество кодирующих бит
+      // поскольку несмотря на то что там и так в большинстве случаев будут нули
+      // все же не исключен случай когда bit_width(n) == sizeof(n) * 8
+      // при котором мы рискуем записать после кодирующих бит пару лишних едениц
+      // в последний октет
+      write((i >= bitWidth) ? 0 : Bit::Get(number, i));
+    }
+  }
 
   inline void write(bool bit) {
     if (index == 64) {
