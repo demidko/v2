8f2db4df0440f17051eda35575fa6374a3b8a533
Daniel Demidko <demidko@farpost.com>
2020-11-22 21:04:52 +1000

    Оптимизируем цикл записи октетов

--- src/VlqOstream.h
+++ src/VlqOstream.h
@@ -12,42 +12,29 @@ struct VlqOstream {
 
   inline ~VlqOstream() { ostream << buffer; }
 
+  /**
+   * Кодирует бит по 7 битным vlq октетам
+   * @param number примитив-число не больше 64 бит
+   */
   template<typename N>
   inline void encode(N number) {
     // количество минимально необходимых для кодирования числа бит (брать по MSB-first)
-    auto len = std::bit_width(number);
-    // количество полных 7 битных октетов в числе
-    auto octets = std::ceil(len / 7.);
-
-    for (auto octets = std::ceil(len / 7.); octets > 0; --octets) {
-      encodeOctet(number,)
+    for (uint16_t i = 0, bitWidth = std::bit_width(number); i < bitWidth;) {
+      // 1 маркирует полный 7 битный октет
+      // 0 маркирует последний, возможно менее чем 7 битный октет
+      write((bitWidth - i) > 7);
+      for (uint16_t limit = i + 7; i < limit; ++i) {
+        // явно заполняем нулями все что выходит за количество кодирующих бит
+        // поскольку несмотря на то что там и так в большинстве случаев будут нули
+        // все же не исключен случай когда bit_width(n) == sizeof(n) * 8
+        // при котором мы рискуем записать после кодирующих бит пару лишних едениц
+        // в последний октет
+        write((i >= bitWidth) ? 0 : Bit::Get(number, i));
+      }
     }
   }
 
 private:
-
-  /**
-   * Кодирует октет
-   * @param number число
-   * @param i текущий индекс бита
-   * @param bitWidth минимально необходимое кол-во бит для кодирования числа
-   * @return i новый индекс бита
-   */
-  template<typename N>
-  inline uint16_t encodeOctet(N number, uint16_t i, uint16_t bitWidth) {
-    // 1 маркирует полный 7 битный октет
-    // 0 маркирует последний, возможно менее чем 7 битный октет
-    write((bitWidth - i) > 7);
-    for (uint16_t limit = i + 7; i < limit; ++i) {
-      // явно заполняем нулями все что выходит за количество кодирующих бит
-      // поскольку несмотря на то что там и так в большинстве случаев будут нули
-      // все же не исключен случай когда bit_width(n) == sizeof(n) * 8
-      // при котором мы рискуем записать после кодирующих бит пару лишних едениц
-      // в последний октет
-      write((i >= bitWidth) ? 0 : Bit::Get(number, i));
-    }
-    return i;
-  }
 
   inline void write(bool bit) {
     if (index == 64) {
